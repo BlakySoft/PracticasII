034c567ed7898208db5e4dcc137545d6c6e17351
BlakySoft <AquinoCristian21@gmail.com>
2025-09-25 18:07:58 -0300

    Ya encontre que estaba pasando con lo del program.cs No se preocupen fue error mio ya esta solucionado

---
+++ CapaPresentacion/Program.cs
@@ -0,0 +1,30 @@
+﻿using LoginTry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (FormLogin login = new FormLogin())
+            {
+                if (login.ShowDialog() == DialogResult.OK)
+                {
+                    Application.Run(new FormMENU());
+                }
+            }
+        }
+    }
+}
