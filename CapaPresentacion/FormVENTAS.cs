344b97eef8378c1eee543fd44422f6d415151d86
BlakySoft <AquinoCristian21@gmail.com>
2025-11-05 00:45:20 -0300

    .

--- CapaPresentacion/FormVENTAS.cs
+++ CapaPresentacion/FormVENTAS.cs
@@ -500,7 +500,7 @@ namespace CapaPresentacion
                             TxtPrecio.Text = Precio.ToString("0,0");
                         }
                         TxtCantidad.Enabled = true;
-                        TxtCantidad.Focus();
+                        //TxtCantidad.Focus();
                     }
                     dr.Close();
                     con.Close();
